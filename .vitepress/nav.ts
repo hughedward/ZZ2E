b338bdcec97cd76b613039e59b5b97359c315370
13MountainsFall <2606927154@qq.com>
2025-03-16 00:45:57 +0800

    hugh: update 08

--- .vitepress/nav.ts
+++ .vitepress/nav.ts
@@ -1,7 +1,11 @@
 export var nav = [
   { text: "Home", link: "/" },
   {
-    text: "rule",
+    text: "社群规则",
     link: "https://kdocs.cn/l/cidM3hs6U5RH"
+  },
+  {
+    text: "初始计划",
+    link: "https://kdocs.cn/l/cdd4gfvZ62FP"
   }
 ]
